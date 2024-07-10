afe23b36c65d1dcb9c13486b731506438c457472
Michael Matheus <michaelmatheus2001@gmail.com>
2024-07-10 18:07:15 -0300

    feat: add routes

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+export const routes = [
+    {
+        method: 'GET',
+        path: '/tasks',
+        handler: (req, res) => {
+
+            return res.end()
+        }
+    },
+    {
+        method: 'POST',
+        path: '/tasks',
+        handler: (req, res) => {
+
+            return res.end()
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/tasks/:id',
+        handler: (req, res) => {
+
+            return res.end()
+        }
+    },
+    {
+        method: 'PATCH',
+        path: '/tasks/:id/complete',
+        handler: (req, res) => {
+
+            return res.end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/tasks/:id',
+        handler: (req, res) => {
+
+            return res.end()
+        }
+    },
+]
