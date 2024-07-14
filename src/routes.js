ed3a6b2b28b61de60064b9d95d22715340afb62e
Michael Matheus <michaelmatheus2001@gmail.com>
2024-07-14 11:21:52 -0300

    feat: add business rules

--- src/routes.js
+++ src/routes.js
@@ -1,42 +1,111 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js"
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
 export const routes = [
     {
         method: 'GET',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
+            const { search } = req.query
+            
+            const tasks = database.select('tasks', search ? {
+                title: search,
+                description: search,
+            } : null)
 
-            return res.end()
+            return res.end(JSON.stringify(tasks))
         }
     },
     {
         method: 'POST',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
+            const { title, description } = req.body
+            
+            const date = new Date().toISOString()
+            
+            const task = {
+                id: randomUUID(),
+                title,
+                description,
+                created_at: date,
+                updated_at: date,
+                completed_at: null,
+            }
+            
+            database.insert('tasks', task)
 
-            return res.end()
+            return res.writeHead(201).end()
         }
     },
     {
         method: 'PUT',
-        path: '/tasks/:id',
+        path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
+            const { id } = req.params
 
-            return res.end()
+            const task = database.select('tasks').find((row) => row.id === id);
+
+            if (!task) {
+                return res.writeHead(404).end()
+            }
+
+            const bodyContent = req.body
+
+            const date = new Date().toISOString()
+
+            const updatedTask = {
+                ...bodyContent,
+                updated_at: date,
+            }
+
+            database.update('tasks', id, updatedTask)
+
+            return res.writeHead(201).end()
         }
     },
     {
         method: 'PATCH',
-        path: '/tasks/:id/complete',
+        path: buildRoutePath('/tasks/:id/complete'),
         handler: (req, res) => {
+            const { id } = req.params
 
-            return res.end()
+            const task = database.select('tasks').find((row) => row.id === id);
+
+            const date = new Date().toISOString()
+
+            if (!task) {
+                return res.writeHead(404).end()
+            }
+
+            const taskCompleteDate = !!task.completed_at
+            ? null : date
+           
+            database.update('tasks', id, {
+                completed_at: taskCompleteDate
+            })
+
+            return res.writeHead(201).end()
         }
     },
     {
         method: 'DELETE',
-        path: '/tasks/:id',
+        path: buildRoutePath('/tasks/:id'),
         handler: (req, res) => {
+            const { id } = req.params
 
-            return res.end()
+            const task = database.select('tasks').find((row) => row.id === id);
+
+            if (!task) {
+                return res.writeHead(404).end()
+            }
+
+            database.delete('tasks', id)
+
+            return res.writeHead(204).end()
         }
     },
 ]
